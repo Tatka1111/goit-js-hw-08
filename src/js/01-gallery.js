caff2f89398c3aa0f7132952efe5ea06c4a5ef17
Nataliia Matsko <mask.dancing11@gmail.com>
2023-03-04 14:35:12 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,7 @@ import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+
 const galleryEl = document.querySelector('.gallery');
 
 galleryEl.insertAdjacentHTML('beforeend', renderGalleryItems());
